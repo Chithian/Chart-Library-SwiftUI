59979834c7cb6230973b957ebcebdda83ed14b87
Chithian Samony <chithiansamony@gmail.com>
2023-01-14 08:56:57 +0700

    Initial Commit

---
+++ Chart Library SwiftUI/Chart_Library_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chart_Library_SwiftUIApp.swift
+//  Chart Library SwiftUI
+//
+//  Created by Samony Chithian on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Chart_Library_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
